ba2f11fecc3f32792a599dc9a839093499ca037d
Tokar <tokarczyk.cyryl2@gmail.com>
2024-04-26 13:47:23 +0200

    implemented key pair generation and encryption of the private key

---
+++ keys_generator.py
@@ -0,0 +1,29 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.ciphers.algorithms import AES
+
+pin = 6969
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=4096
+)
+
+encrypted_pem_private_key = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=AES(pin)
+)
+
+pem_public_key = private_key.public_key().public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+private_key_file = open('encrypted_private_key.pem', 'w')
+private_key_file.write(encrypted_pem_private_key.decode())
+private_key_file.close()
+
+public_key_file = open('public_key.pub', 'w')
+public_key_file.write(pem_public_key.decode())
+public_key_file.close()
